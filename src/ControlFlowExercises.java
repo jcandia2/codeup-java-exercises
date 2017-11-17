4ee57c8611f8e396677e7780a79e696156eee98e
Juan Candia <jcandia2@yahoo.com>
2017-11-17 16:22:56 -0600

    completed letter grade exercise in Control Flow

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -26,29 +26,80 @@ public class ControlFlowExercises {
 //        5      | 25      | 125
 
 
-        int userInput;
+//        int userInput;
+//        String userResponse;
+//
+        Scanner sc = new Scanner(System.in);
+//
+//
+//        do {
+//            System.out.print("Enter an integer: ");
+//            userInput = sc.nextInt();
+//
+//            System.out.println("number | squared | cubed");
+//            System.out.println("------ | ------- | -----");
+//
+//            int squared, cubed;
+//
+//            for (int i = 1; i <= userInput; i++) {
+//                squared = i * i;
+//                cubed = i * i * i;
+//                System.out.printf("%-7d| %-8d| %d\n", i, squared, cubed);
+//            }
+//            System.out.println("Would you like to continue? y/n");
+//            userResponse = sc.next();
+//        } while(userResponse.equals("y"));
+
+
+//        Convert given number grades into letter grades.
+//
+//                Prompt the user for a numerical grade from 0 to 100.
+//        Display the corresponding letter grade.
+//                Prompt the user to continue.
+//        Assume that the user will enter valid integers for the grades.
+//        The application should only continue if the user agrees to.
+//        Grade Ranges:
+//
+//        A : 100 - 88
+//        B : 87 - 80
+//        C : 79 - 67
+//        D : 66 - 60
+//        F : 59 - 0
+//        Bonus
+//
+//        Edit your grade ranges to include pluses and minuses (ex: 99-100 = A+).
+
+        int numericalGrade;
+
         String userResponse;
 
-        Scanner sc = new Scanner(System.in);
-
-
         do {
-            System.out.print("Enter an integer: ");
-            userInput = sc.nextInt();
+            System.out.println("Enter a numerical grade from 0 to 100 and I will display your letter grade.");
+            System.out.println("Enter a numerical grade from 0 to 100: ");
 
-            System.out.println("number | squared | cubed");
-            System.out.println("------ | ------- | -----");
+            numericalGrade = sc.nextInt();
 
-            int squared, cubed;
-
-            for (int i = 1; i <= userInput; i++) {
-                squared = i * i;
-                cubed = i * i * i;
-                System.out.printf("%-7d| %-8d| %d\n", i, squared, cubed);
+            if (numericalGrade <= 100 && numericalGrade >= 88) {
+                System.out.println("Your letter grade is: A");
+            }
+            if (numericalGrade < 88 && numericalGrade >= 80) {
+                System.out.println("Your letter grade is: B");
+            }
+            if (numericalGrade < 80 && numericalGrade >= 67) {
+                System.out.println("Your letter grade is: C");
+            }
+            if (numericalGrade < 67 && numericalGrade >= 60) {
+                System.out.println("Your letter grade is: D");
+            }
+            if (numericalGrade < 60) {
+                System.out.println("Your letter grade is: F");
             }
             System.out.println("Would you like to continue? y/n");
             userResponse = sc.next();
-        } while(userResponse.equals("y"));
+        } while (userResponse.equals("y"));
 
-    }
+
+
+
+        }
 }
