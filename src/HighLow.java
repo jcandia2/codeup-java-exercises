1831982e70af4e01bfedf49b4e9bbe815b5b263d
Juan Candia <jcandia2@yahoo.com>
2017-11-23 23:42:30 -0700

    added a method to display message to player

--- src/HighLow.java
+++ src/HighLow.java
@@ -3,65 +3,72 @@ import java.util.Scanner;
 public class HighLow {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
         int randomNumber = getRandom(1, 100);
         System.out.println(randomNumber);
-        int maxNumberOfGuesses = 10;
+
         int numberOfGuesses = 1;
         String playerName = getPlayerName();
-
+        boolean found = false;
+        String hint;
 
         System.out.print("I'm thinking of a number between 1 and 100. I'll give you 10 tries to guess my number.");
         System.out.println(" What is your first guess: ");
 
         int userGuess = getUserGuess();
 
-        boolean found = false;
-
         while (!found) {
             if (userGuess < randomNumber) {
                 numberOfGuesses += 1;
-                System.out.print(getMessage(numberOfGuesses, maxNumberOfGuesses, playerName) + "HIGHER. Guess Again: ");
-                userGuess = sc.nextInt();
-
+                System.out.println(getMessage(numberOfGuesses, "HIGHER", playerName));
+                userGuess = getUserGuess();
             } else if (userGuess > randomNumber) {
                 numberOfGuesses += 1;
-                System.out.print(getMessage(numberOfGuesses, maxNumberOfGuesses, playerName) + "LOWER. Guess Again: ");
-                userGuess = sc.nextInt();
+                System.out.println(getMessage(numberOfGuesses, "LOWER", playerName));
+                userGuess = getUserGuess();
             } else {
-                System.out.println("LUCKY GUESS.....my number was " + randomNumber);
+                System.out.println("LUCKY GUESS " + playerName + "..... my number was " + randomNumber);
                 found = true;
             }
         }
-
     }
 
-    public static String getMessage(int count, int max, String name) {
-        int remainingGuesses = max - count;
+    public static String getMessage(int count, String hint, String name) {
+        int maxNumberOfGuesses = 10;
+        int remainingGuesses = maxNumberOfGuesses - count;
         String message;
         switch (remainingGuesses) {
-            case 9:  message = "First guess. You have to go ";
+            case 9:
+                message = "First guess. You have to go " + hint + ". Guess Again: ";
                 break;
-            case 8:  message = "Second guess. You have to go ";
+            case 8:
+                message = "Second guess. You have to go " + hint + ". Guess Again: ";
                 break;
-            case 7:  message = "Third guess. You have to go ";
+            case 7:
+                message = "Third guess. You have to go " + hint + ". Guess Again: ";
                 break;
-            case 6:  message = "Fourth guess. You have to go ";
+            case 6:
+                message = "Fourth guess. You have to go " + hint + ". Guess Again: ";
                 break;
-            case 5:  message = "That's your fifth guess " + name +  ". You have to go ";
+            case 5:
+                message = "That's your fifth guess " + name + ". You have to go " + hint + ". Guess Again: ";
                 break;
-            case 4:  message = "NOPE! 4 guesses remaining. You have to go ";
+            case 4:
+                message = "NOPE! 4 guesses remaining. You have to go " + hint + ". Guess Again: ";
                 break;
-            case 3:  message = "3 more guesses " + name + ". You have to go ";
+            case 3:
+                message = "3 more guesses " + name + ". You have to go " + hint + ". Guess Again: ";
                 break;
-            case 2:  message = "2 left. You have to go ";
+            case 2:
+                message = "2 left. You have to go " + hint + ". Guess Again: ";
                 break;
-            case 1:  message = "last 1. You have to go ";
+            case 1:
+                message = "last 1. You have to go " + hint + ". Guess Again: ";
                 break;
-            case 0: message = "guess. You have to go ";
+            case 0:
+                message = "guess. You have to go " + hint + ". Guess Again: ";
                 break;
-            default: message = "I don't know what happened!!!";
+            default:
+                message = "I don't know what happened!!!";
                 break;
         }
         return message;
