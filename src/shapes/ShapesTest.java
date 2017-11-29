af945ac2cacb6b2742b0f1c580df912685b80afb
Juan Candia <jcandia2@yahoo.com>
2017-11-29 11:52:44 -0600

    add inheritance and polymorphism exercises ShapesTest, Square and Rectangle class

---
+++ src/shapes/ShapesTest.java
@@ -0,0 +1,17 @@
+package shapes;
+
+public class ShapesTest {
+    public static void main(String[] args) {
+        Rectangle box1 = new Rectangle(5,4);
+        System.out.println(box1.getArea());
+        System.out.println(box1.getPerimeter());
+
+
+        // create a variable of the type Rectangle named box2 and assign it a new
+        // instance of the Square class that has a side value of 5 - this is polymorphism, it doesn't care
+        // what type it is as long as it is of type child or parent
+        Rectangle box2 = new Square(5);
+        System.out.println(box2.getArea());
+        System.out.println(box2.getPerimeter());
+    }
+}
---
+++ src/shapes/Square.java
@@ -0,0 +1,23 @@
+package shapes;
+
+public class Square extends Rectangle {
+    //    Square should define a constructor that accepts one argument, side, and calls the parent's constructor to
+//    set both the length and width to the value of side.
+    public double side;
+
+    public Square(double side) {
+        super(side, side);
+        this.side = side;
+    }
+
+    public double getArea() {
+        System.out.println("calculating area from the Square class:");
+        return Math.pow(this.side, 2);
+    }
+
+    public double getPerimeter() {
+        System.out.println("calculating perimeter from the Square class:");
+        return 4 * this.side;
+    }
+
+}
