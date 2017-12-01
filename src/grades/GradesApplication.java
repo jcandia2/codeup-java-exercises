a1c0c2cee26b0a27a03df8b55f640ff8f803fe51
Juan Candia <jcandia2@yahoo.com>
2017-12-01 11:40:40 -0600

    add main menu to allow user to show all student's

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -7,11 +7,12 @@ import java.util.HashMap;
 public class GradesApplication {
 
     public static void main(String[] args) {
-
         Input input = new Input();
+
         String userInput;
         boolean userResponse = true;
         boolean validUserName = true;
+        int userOption;
 
         HashMap<String, Student> students = new HashMap<>();
 
@@ -30,50 +31,63 @@ public class GradesApplication {
         student3.addGrade(70);
         student3.addGrade(90);
 
-        System.out.println("The average for " + student1.getName() + " is " + student1.getGradeAverage());
-        System.out.println("The average for " + student2.getName() + " is " + student2.getGradeAverage());
-        System.out.println("The average for " + student3.getName() + " is " + student3.getGradeAverage());
-
         students.put("bobsUserName", student1);
         students.put("juliesUserName", student2);
         students.put("billsUserName", student3);
 
 
         showMainMenu();
-        showMenu(students);
+        userOption = getUserOption();
 
-        do {
-            do {
-                System.out.println("What student would you like to see more information on?");
-                System.out.println();
-                System.out.print(">");
-                userInput = input.getString();
-                if (students.containsKey(userInput)) {
-                    displayStudentInfo(students, userInput);
-                } else {
-                    System.out.println("Sorry, no student found with the github username of " + userInput);
+        if (userOption == 1) {
+            showMenu(students);
+            do {            //do while userResponse = y
+                do {                //do while invalid userName entered
+                    System.out.println("What student would you like to see more information on?");
                     System.out.println();
-                    validUserName = false;
-                }
-                System.out.println("Would you like to see another student?");
-                System.out.println();
-                System.out.print(">");
-                userResponse = input.yesNo();
-                input.getString();
-            } while (!validUserName && userResponse);
-        } while (userResponse);
+                    System.out.print(">");
+                    userInput = input.getString();
+                    if (students.containsKey(userInput)) {
+                        displayStudentInfo(students, userInput);
+                    } else {
+                        System.out.println("Sorry, no student found with the github username of " + userInput);
+                        System.out.println();
+                        validUserName = false;
+                    }
+                    System.out.println("Would you like to see another student?");
+                    System.out.println();
+                    System.out.print(">");
+                    userResponse = input.yesNo();
+                    input.getString();
+                } while (!validUserName && userResponse);
+            } while (userResponse);
 
-        System.out.println("Ok bye!");
+            System.out.println("Ok bye!");
+        } else if (userOption == 2) {
+            showAllStudents(students);
+        } else {
+            System.out.println("Ok bye!");
+        }
     }
 
+
+    public static int getUserOption() {
+        Input input = new Input();
+        return input.getInt(1, 3);
+    }
 
     public static void showMainMenu() {
         System.out.println("Welcome!");
         System.out.println();
-        System.out.println("Please make your selection: [1 or 2]");
+        System.out.println("Please make your selection: [1 or 2 or 3]");
+        System.out.println();
         System.out.println("1. To view a student's information.");
         System.out.println("2. To display all students.");
+        System.out.println("3. Quit.");
+        System.out.println();
+        System.out.print(">");
     }
+
     public static void showMenu(HashMap<String, Student> students) {
         System.out.println("Here are the github usernames of our students:");
         System.out.println();
@@ -92,5 +106,12 @@ public class GradesApplication {
         students.get(userInput).getGrades();
         System.out.println("Current Average: " + students.get(userInput).getGradeAverage());
         System.out.println();
+    }
+
+    public static void showAllStudents(HashMap<String, Student> students) {
+        for (String key : students.keySet()) {
+            displayStudentInfo(students, key);
+        }
+
     }
 }
